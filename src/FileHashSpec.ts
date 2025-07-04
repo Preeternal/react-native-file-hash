1909cd2074ec2ab20775b3de71bdc4b49b48579e
Preeternal <preeternal@gmail.com>
2025-07-04 17:56:44 +0300

    setup eslint & prettier

--- src/FileHashSpec.ts
+++ src/FileHashSpec.ts
@@ -2,8 +2,8 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  getFileSha256(filePath: string): Promise<string>;
-  md5Hash(filePath: string): Promise<string>;
+    getFileSha256(filePath: string): Promise<string>;
+    md5Hash(filePath: string): Promise<string>;
 }
 
 export default TurboModuleRegistry.get<Spec>('FileHash');
