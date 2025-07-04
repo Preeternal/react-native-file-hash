e1a5f43fb05136ac6fb6b014d4be1a5506c27b42
Preeternal <preeternal@gmail.com>
2025-07-04 18:14:52 +0300

    build: add lib directory to gitignore

--- lib/FileHashSpec.d.ts
+++
@@ -1,7 +0,0 @@
-import type { TurboModule } from 'react-native';
-export interface Spec extends TurboModule {
-    getFileSha256(filePath: string): Promise<string>;
-    md5Hash(filePath: string): Promise<string>;
-}
-declare const _default: Spec | null;
-export default _default;
--- lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getFileSha256(filePath: string): Promise<string>;
-export declare function md5Hash(filePath: string): Promise<string>;
